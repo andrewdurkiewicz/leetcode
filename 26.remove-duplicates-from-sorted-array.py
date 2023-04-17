0b1c7c3e5c04a0a86dcadfada47570c3604f8bbc
andrew <andewdurkiewicz@gmail.com>
2023-04-17 10:57:06 -0700

    26

--- 26.remove-duplicates-from-sorted-array.py
+++ 26.remove-duplicates-from-sorted-array.py
@@ -5,8 +5,21 @@
 #
 
 # @lc code=start
+
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        unique = 0 # number of unique index
+        index = 0
+        while index < len(nums):
+            curr_value = nums.pop(index)
+            unique+=1
+            while index < len(nums) and nums[index] == curr_value:
+                nums.pop(index)
+            nums.insert(index, curr_value)
+            index+=1
+        return unique
+                
+
         
 # @lc code=end
 
