fa1736b96a2636fa6882d5e36d90a5e6f28997f1
adurkiewicz <andrewdurkiewicz@gmail.com>
2023-05-19 20:01:24 -0700

    22

---
+++ 22.generate-parentheses.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=22 lang=python3
+#
+# [22] Generate Parentheses
+#
+
+# @lc code=start
+# from typing import List
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        ret = []
+        def backTrack(currStr, numL, numR, n):
+            if numR > numL:
+                return
+            if len(currStr) == 2*n:
+                #We have hit the end
+                if (numL == n) and (numR == n):
+                    ret.append(currStr)
+            else:
+                backTrack(currStr + "(", numL+1, numR, n)
+                backTrack(currStr + ")", numL, numR+1, n)
+        backTrack("", 0, 0, n)
+
+        return ret
+                
+
+# s = Solution()
+# print(s.generateParenthesis(3))
+# @lc code=end
+
