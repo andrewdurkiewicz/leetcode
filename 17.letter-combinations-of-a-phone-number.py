2158387e1bf86999bd408bb6d8bf42cfed37850b
adurkiewicz <andrewdurkiewicz@gmail.com>
2023-05-19 17:48:04 -0700

    17

--- 17.letter-combinations-of-a-phone-number.py
+++ 17.letter-combinations-of-a-phone-number.py
@@ -5,35 +5,29 @@
 #
 
 # @lc code=start
-digit_dict = {
-    2 : ["a", "b", "c"],
-    3 : ["d", "e", "f"],
-    4 : ["g", "h", "i"],
-    5 : ["j", "k", "l"],
-    6 : ["m", "n", "o"],
-    7 : ["p", "q", "r", "s"],
-    8 : ["t", "u", "v"],
-    9 : ["w", "x", "y", "z"]
-}
 
-# from typing import List
+
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if digits == "":
-            return []
-        curr = [""]
-        for digit in digits:
-            asInt = int(digit)
-            Options = digit_dict[asInt]
-            tmp = curr
-            curr = []
-            for val in tmp:
-                for addthis in Options:
-                    curr.append(val+addthis)
-        return curr
+        res = []
+        digitToChar = {"2" : ["a", "b", "c"],
+                       "3" : ["d", "e", "f"],
+                       "4" : ["g", "h", "i"],
+                       "5" : ["j", "k", "l"],
+                       "6" : ["m", "n", "o"],
+                       "7" : ["p", "q", "r", "s"],
+                       "8" : ["t", "u", "v"],
+                       "9" : ["w", "x", "y", "z"]}
+        def backtrack(i, currStr):
+            if len(currStr) == len(digits):
+                res.append(currStr)
+                return
+            for c in digitToChar[digits[i]]:
+                backtrack(i + 1, currStr + c)
+        if digits:
+            backtrack(0, "")
+        return res
 
-                
-# s = Solution() 
-# print(s.letterCombinations(""))
+            
 # @lc code=end
 
